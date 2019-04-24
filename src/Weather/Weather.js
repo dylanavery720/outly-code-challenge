6619e4c8f92d1d22e81ed3d07e2c3011f43829cf
Dylan A <dylanpatrickavery@gmail.com>
2019-04-24 14:03:30 -0600

    Linting

--- src/Weather/Weather.js
+++ src/Weather/Weather.js
@@ -2,18 +2,17 @@ import React, { Component } from 'react';
 import './Weather.css';
 
 class Weather extends Component {
-
   render() {
     const { name, wind, weather, main } = this.props.weather;
     return (
-        <div>
-          <p className="Weather-title name">{name === "" ? "Uncharted" : name}</p>
-          <p className="Weather-title">Humidity: {main.humidity}%</p>
-          <p className="Weather-title">Forecast: {weather[0].main}</p>
-          <p className="Weather-title">Wind Speed: {wind.speed}mph</p>
-          <p className="Weather-title high">High: {main.temp_max}°</p>
-          <p className="Weather-title low">Low: {main.temp_min}°</p>
-        </div>
+      <div>
+        <p className="Weather-title name">{name === '' ? 'Uncharted' : name}</p>
+        <p className="Weather-title">Humidity: {main.humidity}%</p>
+        <p className="Weather-title">Forecast: {weather[0].main}</p>
+        <p className="Weather-title">Wind Speed: {wind.speed}mph</p>
+        <p className="Weather-title high">High: {main.temp_max}°</p>
+        <p className="Weather-title low">Low: {main.temp_min}°</p>
+      </div>
     );
   }
 }
